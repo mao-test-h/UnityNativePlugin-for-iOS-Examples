634b66241d63e3f418c9894141fef7cf42e277d9
mao <miymao78.d@gmail.com>
2021-03-07 10:56:13 +0900

    feat: Change MinimumExample

--- Assets/MinimumExample/Scripts/Example.cs
+++ Assets/MinimumExample/Scripts/Example.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
 using UnityEngine.UI;
-using MinimumExample.iOS;
 
 namespace MinimumExample
 {
+    /// <summary>
+    /// 最小構成のサンプル呼び出し
+    /// </summary>
     sealed class Example : MonoBehaviour
     {
         [SerializeField] Button _buttonHelloWorld = default;
@@ -12,10 +15,37 @@ namespace MinimumExample
         {
             _buttonHelloWorld.onClick.AddListener(() =>
             {
+#if !UNITY_EDITOR && UNITY_IOS
                 // プラグインの呼び出し
-                var ret = ExampleBridge.PrintHelloWorld();
+                var ret = PrintHelloWorld();
                 Debug.Log($"戻り値: {ret}");
+#else
+                // それ以外のプラットフォームからの呼び出し (Editor含む)
+                Debug.Log("Hello World (iOS以外からの呼び出し)");
+#endif
             });
         }
+
+        #region P/Invoke
+
+        // ObjectiveC++コードで実装した`Example`クラスのP/Invoke
+
+        /// <summary>
+        /// `printHelloWorld`の呼び出し
+        /// </summary>
+        /// <remarks>
+        /// NOTE: Example.mmの「extern "C"」内で宣言した関数をここで呼び出す
+        /// - iOSのプラグインは静的に実行ファイルにリンクされるので、`DllImport`にはライブラリ名として「__Internal」を指定する必要がある
+        /// - `EntryPoint`に.mm側で宣言されている名前を渡すことでC#側のメソッド名は別名を指定可能
+        /// </remarks>
+        [DllImport("__Internal", EntryPoint = "printHelloWorld")]
+        static extern int PrintHelloWorld();
+
+
+        // NOTE: ちなみに`EntryPoint`を指定しない場合は、以下のようにC#側も.mm側と同名に合わせる必要がある
+        // [DllImport("__Internal")]
+        // static extern int printHelloWorld();
+
+        #endregion P/Invoke
     }
 }
