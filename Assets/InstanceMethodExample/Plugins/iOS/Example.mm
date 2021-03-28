9092ecf5b9b2f0daed90732af8bb9a02be866650
mao <miymao78.d@gmail.com>
2021-03-29 08:26:30 +0900

    feat: add instanceMethod example.

---
+++ Assets/InstanceMethodExample/Plugins/iOS/Example.mm
@@ -0,0 +1,82 @@
+#import <Foundation/Foundation.h>
+
+// MARK:- interface (クラスの宣言部)
+
+@interface Example : NSObject
+
+// メンバ変数に値を設定
+- (void)setMember:(int)value;
+
+// ログに"Hello World"と出力してメンバ変数(_member)に設定された値を返す
+- (int)printHelloWorldWithMember;
+
+@end
+
+
+// MARK:- implementation (クラスの実装部)
+
+@implementation Example {
+
+    // メンバ変数
+    int _member;
+}
+
+// イニシャライザ(インスタンスの初期化)
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _member = 0;
+    }
+
+    return self;
+}
+
+- (void)setMember:(int)value {
+    _member = value;
+}
+
+- (int)printHelloWorldWithMember {
+    // ログ出力
+    NSLog(@"Hello World : [%d]", _member);
+
+    // 戻り値を返す
+    return _member;
+}
+
+@end
+
+
+// MARK:- extern "C" (Cリンケージで宣言)
+// NOTE: ここで外部宣言している関数が実際にUnity(C#)から呼び出される
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// インスタンス化
+// NOTE: 戻り値のポインタをC#側でIntPtrなどで保持し、インスタンスメソッドの呼び出し時に渡して使う
+Example* createExample() {
+    Example* instance = [[Example alloc] init];
+    CFRetain((CFTypeRef) instance);
+    return instance;
+}
+
+// 解放
+void releaseExample(Example* instance) {
+    CFRelease((CFTypeRef) instance);
+}
+
+// 以下はインスタンスメソッドの呼び出し
+// NOTE: 第一引数にはインスタンス化した際に保持しているポインタを渡す
+
+void setMember(Example* instance, int value) {
+    [instance setMember:value];
+}
+
+int printHelloWorldWithMember(Example* instance) {
+    return [instance printHelloWorldWithMember];
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ Assets/InstanceMethodExample/Scripts/Example.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace InstanceMethodExample
+{
+    /// <summary>
+    /// インスタンス化及びインスタンスメソッドの呼び出しサンプル
+    /// </summary>
+    sealed class Example : MonoBehaviour
+    {
+        [SerializeField] Button _buttonHelloWorld = default;
+        [SerializeField] InputField _inputField = default;
+
+        IntPtr _instance = IntPtr.Zero;
+
+        void Start()
+        {
+            _buttonHelloWorld.onClick.AddListener(() =>
+            {
+#if !UNITY_EDITOR && UNITY_IOS
+                // プラグインの呼び出し
+                var ret = PrintHelloWorldWithMember(_instance);
+                Debug.Log($"戻り値: {ret}");
+#else
+                // それ以外のプラットフォームからの呼び出し (Editor含む)
+                Debug.Log("Hello World (iOS以外からの呼び出し)");
+#endif
+            });
+
+            _inputField.onEndEdit.AddListener(text =>
+            {
+                if (int.TryParse(text, out var num))
+                {
+#if !UNITY_EDITOR && UNITY_IOS
+                    // プラグインの呼び出し
+                    SetMember(_instance, num);
+#else
+                    Debug.Log($"{num} (iOS以外からの呼び出し)");
+#endif
+                }
+            });
+
+#if !UNITY_EDITOR && UNITY_IOS
+            _instance = CreateExample();
+#endif
+        }
+
+        void OnDestroy()
+        {
+            if (_instance != IntPtr.Zero)
+            {
+                ReleaseExample(_instance);
+            }
+        }
+
+        #region P/Invoke
+
+        // ObjectiveC++コードで実装した`Example`クラスのP/Invoke
+
+        // ネイティブコード側にあるExampleクラスのインスタンス化
+        // NOTE: 戻り値はインスタンスのポインタ
+        [DllImport("__Internal", EntryPoint = "createExample")]
+        static extern IntPtr CreateExample();
+
+        // インスタンスの解放
+        [DllImport("__Internal", EntryPoint = "releaseExample")]
+        static extern void ReleaseExample(IntPtr instance);
+
+        // `setMember`の呼び出し
+        [DllImport("__Internal", EntryPoint = "setMember")]
+        static extern int SetMember(IntPtr instance, int value);
+
+        // `printHelloWorldWithMember`の呼び出し
+        [DllImport("__Internal", EntryPoint = "printHelloWorldWithMember")]
+        static extern int PrintHelloWorldWithMember(IntPtr instance);
+
+        #endregion P/Invoke
+    }
+}
